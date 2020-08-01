5c4b76c5b8066c743ffd97d22ccd417b7ae591bd
desertlp <lorapaine1@gmail.com>
2020-08-01 10:53:19 -0700

    index page card loop working

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,15 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # HOME PAGE
     path('', views.home, name='home'),
-        # name='home' kwarg (aka an alias)
-    # ABOUT PAGE
     path('about/', views.about, name='about'),
-    # SKILLS PAGE
     path('skills/', views.skills, name='skills'),
-    # PROJECTS PAGE
-    path('projects/', views.projects, name='projects'),
-    # CONTACT PAGE
+    path('index/', views.index, name='index'),
     path('contact/', views.contact, name='contact'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,43 +1,29 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 
-# TEMPORARY PROJECTS DATA 
-class Project:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, project_url, company, description, timeframe, deployment_date):
+
+
+class Project:
+  def __init__(self, name):
     self.name = name
-    self.project_url = project_url
-    self.company = company
-    self.description = description
-    self.timeframe = timeframe
-    self.deployment_date = deployment_date
 
 projects = [
-  Project('Takeuchi Tamagotchi', 'https://desertlp.github.io/tamagotchi/', 'General Assembly', 'first project', '72 hours', '6-26-2020'),
-  Project('PartyWave', 'https://desertlp.github.io/PartyWave/', 'General Assembly', 'first team project', '5 days', '7-15-2020'),
+  Project('Takeuchi Tamagotchi'), 
+  Project('PartyWave')
 ]
 
 
-# HOME 
 def home(request):
-    # return HttpResponse('get ready bitch, its portfolio time')
     return render(request, 'home.html')
 
 def about(request):
-    # return HttpResponse('about me page')
     return render(request, 'about.html')
  
 def skills(request): 
-    # return HttpResponse('list ya skills out hoe')
     return render(request, 'skills.html')
 
-def projects(request): 
-    # projects = Project.objects.all()
-    context = {
-        'projects': projects
-    }
-    return render (request, 'index.html', context)
-    # return render(request, 'cats/index.html', context)
+# projects page
+def index(request):
+    return render(request, 'index.html', { 'projects': projects })
 
 def contact(request): 
-    # return HttpResponse('this should be a form')
     return render(request, 'contact.html')
