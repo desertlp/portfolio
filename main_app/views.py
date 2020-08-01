413c40bf40f0e70b1c7956e0739309f7ad29061b
desertlp <lorapaine1@gmail.com>
2020-08-01 12:36:59 -0700

    templates/basics done, bootstrap and css connected

--- main_app/views.py
+++ main_app/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render, HttpResponse, redirect
 
-
-
 class Project:
-  def __init__(self, name):
+  def __init__(self, name, project_url, description, deployment_date):
     self.name = name
+    self.project_url = project_url
+    self.description = description
+    self.deployment_date = deployment_date
 
 projects = [
-  Project('Takeuchi Tamagotchi'), 
-  Project('PartyWave')
+  Project('Takeuchi Tamagotchi', 'https://desertlp.github.io/tamagotchi/', 'first project, 3 days, solo', 'June 26th, 2020'), 
+  Project('PartyWave', 'https://secure-mountain-38318.herokuapp.com/', 'second project, 5 days, team project will adams', 'July 15th, 2020' ),
 ]
 
 
