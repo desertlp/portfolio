c1a0729cb2c107c1f7321faedda0b2a96c77507a
desertlp <lorapaine1@gmail.com>
2020-07-31 23:53:13 -0700

    tried to make index for loop work, not rendering

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# TEMPORARY PROJECTS DATA 
+class Project:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, name, project_url, company, description, timeframe, deployment_date):
+    self.name = name
+    self.project_url = project_url
+    self.company = company
+    self.description = description
+    self.timeframe = timeframe
+    self.deployment_date = deployment_date
+
+projects = [
+  Project('Takeuchi Tamagotchi', 'https://desertlp.github.io/tamagotchi/', 'General Assembly', 'first project', '72 hours', '6-26-2020'),
+  Project('PartyWave', 'https://desertlp.github.io/PartyWave/', 'General Assembly', 'first team project', '5 days', '7-15-2020'),
+]
+
+
 # HOME 
 def home(request):
     # return HttpResponse('get ready bitch, its portfolio time')
@@ -15,8 +31,12 @@ def skills(request):
     return render(request, 'skills.html')
 
 def projects(request): 
-    # return HttpResponse('loop throught the project db')
-    return render (request, 'index.html')
+    # projects = Project.objects.all()
+    context = {
+        'projects': projects
+    }
+    return render (request, 'index.html', context)
+    # return render(request, 'cats/index.html', context)
 
 def contact(request): 
     # return HttpResponse('this should be a form')
