ec7b526924c9b822e7b0e3d049c9b5847d9bb512
desertlp <lorapaine1@gmail.com>
2020-08-01 20:35:49 -0700

    added second model and admin capability

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+TYPES = (
+  ('lang','Language'),
+  ('frmwk', 'Framework'),
+  ('db','Database'),
+  ('styl','Styling'),
+)
+
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+    project_url = models.URLField(max_length=250)
+    # CharField uses a <input type="text"> as its widget, whereas, a TextField uses a <textarea>
+    description = models.TextField(max_length=500)
+    deployment_date = models.DateField()
+        # format should be YYYY-MM-DD
+
+    def __str__(self):
+        return self.name 
+
+class Skill(models.Model):
+    name = models.CharField(max_length=100)
+    skill_type = models.CharField(
+        'Skill Type',
+        # Adds a drop down for skill_type option (language, framework, tool, special)
+        max_length = 100,
+        choices=TYPES,
+            # Sets enumerater option for skill_type choices from tuples 
+        # Set Default Type
+        default=TYPES[0][0],
+            # default to language 
+    )
+    def __str__(self):
+        return self.name 
+            # Nice method for obtaining the friendly value of a Field.choice
--- main_app/views.py
+++ main_app/views.py
@@ -1,17 +1,5 @@
 from django.shortcuts import render, HttpResponse, redirect
-
-class Project:
-  def __init__(self, name, project_url, description, deployment_date):
-    self.name = name
-    self.project_url = project_url
-    self.description = description
-    self.deployment_date = deployment_date
-
-projects = [
-  Project('Takeuchi Tamagotchi', 'https://desertlp.github.io/tamagotchi/', 'first project, 3 days, solo', 'June 26th, 2020'), 
-  Project('PartyWave', 'https://secure-mountain-38318.herokuapp.com/', 'second project, 5 days, team project will adams', 'July 15th, 2020' ),
-]
-
+from .models import Project, Skill
 
 def home(request):
     return render(request, 'home.html')
@@ -20,11 +8,19 @@ def about(request):
     return render(request, 'about.html')
  
 def skills(request): 
-    return render(request, 'skills.html')
+    skills = Skill.objects.all()
+    context = {
+        'skills': skills
+    }
+    return render(request, 'skills.html', context)
 
-# projects page
+# Projects Page
 def index(request):
-    return render(request, 'index.html', { 'projects': projects })
+    projects = Project.objects.all()
+    context = {
+        'projects': projects
+    }
+    return render(request, 'index.html', context)
 
 def contact(request): 
     return render(request, 'contact.html')
