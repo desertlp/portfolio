f13a057282682eb7e1395d0bfc1b069a57b433c2
desertlp <lorapaine1@gmail.com>
2020-08-02 19:30:02 -0700

    skill-specific page (one to many)

--- main_app/models.py
+++ main_app/models.py
@@ -21,7 +21,7 @@ class Skill(models.Model):
     )
     def __str__(self):
         # Nice method for obtaining the friendly value of a Field.choice
-        return f"{self.get_skill_type_display()}"
+        return self.name
 
 
 class Project(models.Model):
@@ -32,14 +32,26 @@ class Project(models.Model):
     deployment_date = models.DateField()
         # format should be YYYY-MM-DD
     # Create a skill_id FK
-    skill = models.ForeignKey(Skill, on_delete=models.CASCADE)
+    skill = models.ForeignKey(Skill, default="", on_delete=models.CASCADE)
         # ForeignKey field-type is used to create a one-to-many relationship.
         # Since a project belong to a skill set, project must hold the id of the skill object it belongs to, aka foreign key
-
+        # on_delete=models.CASCADE is required. It ensures that if a Cat record is deleted, all of the child Feedings will be deleted automatically as well - thus avoiding orphan records 
     def __str__(self):
         return self.name 
 
 
 
-
+class Contact(models.Model): 
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    company = models.CharField(max_length=100, default="")
+    email = models.EmailField(max_length=150)
+        # https://docs.djangoproject.com/en/2.2/ref/validators/#django.core.validators.EmailValidator
+    message = models.TextField(max_length=500, default="")
+    schedule_meeting = models.DateField()
+        # make this pop up if you click a button and want to schedule a time to talk, link to google calendar?? 
+    submition_date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.email
 
